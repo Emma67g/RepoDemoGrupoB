c272e659bdebafc35eeb743ac18a74e551b103ff
Emma <emma@ucc.mx>
2026-04-30 19:37:34 -0600

    Routes

---
+++ Demo_React/DemoSesion/src/protectedRoute/useAuth.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import { useActionData } from "react-router-dom";
+
+export function useAuth(){
+    const checkAuth = async()=>{
+        console.log("Ejecutando checkAuth");
+        try{
+            const res= await fetch("http://localhost:3000/perfil",{
+                credentials: "include"
+            });
+    
+            if(!res.ok) throw new Error();
+            const data= await res.json();
+            setUser(data.user);
+        }catch{
+            setUser(null);
+        }finally{
+            setLoading(false);
+        }
+    }
+    useEffect(()=>{
+        checkAuth();
+    },[]);
+        
+    return { user, loading, checkAuth };
+}
+
